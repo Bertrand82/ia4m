06642b627716e1b5b02596f0c1fabee4654d9784
Bertrand82 <bertrand.guiral@gmail.com>
2025-12-18 15:53:59 +0100

    response

--- src/app/component-email-response/component-email-response.ts
+++ src/app/component-email-response/component-email-response.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { BgMail } from '../modeles/BgMail';
+import { GisGmailServiceHelper } from '../services/gis-gmail.service.helper';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'email-response',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './component-email-response.html',
   styleUrl: './component-email-response.scss',
 })
 export class ComponentEmailResponse {
+
+  @Input() selectedEmail: BgMail | null = null;
+  role: string = 'ami'; // Par défaut, le rôle est 'ami'
+  lengthResponse: number = 100; // Longueur par défaut de la réponse
+
+  constructor(private gmailHelper: GisGmailServiceHelper, public changeDetectorRef: ChangeDetectorRef) { } 
+
 
 }
